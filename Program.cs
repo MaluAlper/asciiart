24822312cff124eafac5b41b36b88c273c9ab843
Maria Luiza <malualper@hotmail.com>
2020-05-07 23:19:54 -0300

    Ascii

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace asciiart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ||  ||");
+            Console.WriteLine(" \\\\()//");
+            Console.WriteLine(" //(_)\\\\");
+            Console.WriteLine(" ||   ||");
+            
+            
+        }
+    }
+}
